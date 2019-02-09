adcba02f07f50847d076d04dd7cf677945841673
zeeket <z33k@bitmai.la>
2019-02-09 21:08:13 +0200

    toiminnallisuutta ryhmiin 1

--- application/groups/models.py
+++ application/groups/models.py
@@ -2,10 +2,17 @@ from application import db
 from application.models import Base
 
 class Group(Base):
+    __tablename__ = 'group'
     name = db.Column(db.String(144), nullable=False)
     created_by = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
+    group_users = relationship("Group_user", cascade="save-update, merge, delete")
 
     def __init__(self, name):
         #t√m√§konstruktori tulee joskus ehk√ saamaan ryhm√n j√senet listana
         self.name = name
+
+group_accounts = Table('group_accounts', Base.metadata,
+                        Column('group_id', Integer, ForeignKey('group.id')),
+                        Column('account_id', Integer, ForeignKey('account.id'))
+                      )
--- application/groups/views.py
+++ application/groups/views.py
@@ -5,34 +5,38 @@ from application.groups.models import Group
 from application.groups.forms import GroupForm
 
 @app.route("/groups", methods=["GET"])
-def polls_index():
-    return render_template("polls/list.html", polls = Poll.query.all())
-
-@app.route("/polls/new/")
 @login_required
-def polls_form():
-    return render_template("polls/new.html", form = PollForm())
+def groups_index():
+    return render_template("groups/list.html",
+           groups = Group.query.filter_by(created_by=current_user.id).all())
 
-@app.route("/polls/<poll_id>/", methods=["POST"])
+@app.route("/groups/new/")
 @login_required
-def polls_set_done(poll_id):
+def groups_form():
+    return render_template("groups/new.html", form = GroupForm())
 
-    p = Poll.query.get(poll_id)
-    p.done = True
-    db.session().commit()
+@app.route("/groups/<group_id>/", methods=["POST"])
+@login_required
+def groups_remove(group_id):
+
+    g = Group.query.get(group_id)
+    if g.created_by==current_user.id:
+        db.delete(g)
+        db.session().commit()
 
     return redirect(url_for("polls_index"))
 
 @app.route("/polls/", methods=["POST"])
 @login_required
-def polls_create():
-    form = PollForm(request.form)
+def groups_create():
+    form = GroupForm(request.form)
 
     if not form.validate():
-        return render_template("polls/new.html", form = form)
+        return render_template("groups/new.html", form = form)
 
-    p = Poll(form.name.data,form.description.data)
+    g = Group(form.name.data)
     p.account_id = current_user.id
+    #INSERT INTO group_accounts VALUES :group_id, :account_id
 
     db.session().add(p)
     db.session().commit()
