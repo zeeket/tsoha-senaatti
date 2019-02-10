0854b42522f58a9d802e0f207fb85c693715a28d
zeeket <z33k@bitmai.la>
2019-02-10 20:24:14 +0200

    lisaa toiminnallisuutta, aanestaminen

--- application/polls/forms.py
+++ application/polls/forms.py
@@ -4,6 +4,12 @@ from wtforms import StringField,validators
 class PollForm(FlaskForm):
     name = StringField("Senaatin nimi*", [validators.Length(min=3)])
     description = StringField("Pidempi kuvaus senaatille")
- 
+
     class Meta:
         csrf = False
+
+class SinglePollForm(FlaskForm):
+    upvote = SubmitField()
+    neutralvote = SubmitField()
+    downvote = SubmitField()
+
--- application/polls/views.py
+++ application/polls/views.py
@@ -13,7 +13,7 @@ def polls_index():
 def polls_form():
     return render_template("polls/new.html", form = PollForm())
 
-@app.route("/polls/<poll_id>/", methods=["POST"])
+@app.route("/polls/<poll_id>/done/", methods=["POST"])
 @login_required
 def polls_set_done(poll_id):
 
@@ -38,3 +38,44 @@ def polls_create():
     db.session().commit()
 
     return redirect(url_for("polls_index"))
+
+@app.route("/polls/<poll_id>/", methods=["POST", "GET"])
+def polls_singleview(poll_id):
+    p = Poll.query.get(poll_id)
+    return render_template("polls/singleview.html", poll=p,
+                           form = SinglePollForm())
+
+@app.route("/polls/<poll_id>/up", methods=["POST"])
+@login_required
+def polls_set_upvote(poll_id):
+
+    p = Poll.query.get(poll_id)
+    if not p.account_id == current_user.id and p.has_voted(current_user):
+        p.upvotes+=1
+        db.session().commit()
+
+    return redirect(url_for("polls_singleview"))
+
+@app.route("/polls/<poll_id>/neutral", methods=["POST"])
+@login_required
+def polls_set_neutralvote(poll_id):
+
+    p = Poll.query.get(poll_id)
+    if not p.account_id == current_user.id and p.has_voted(current_user):
+        p.neutralvotes+=1
+        db.session().commit()
+
+    return redirect(url_for("polls_singleview"))
+
+@app.route("/polls/<poll_id>/down", methods=["POST"])
+@login_required
+def polls_set_downvote(poll_id):
+
+    p = Poll.query.get(poll_id)
+    if not p.account_id == current_user.id and p.has_voted(current_user):
+        p.downvotes+=1
+        db.session().commit()
+
+    return redirect(url_for("polls_singleview"))
+
+
